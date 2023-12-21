a1d48e4171b4a272306213530dee76b79dfea245
ctomrp <cristian.rico.palma@gmail.com>
2023-12-21 19:37:04 -0300

    custom user model

---
+++ src/apps/user/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self,
+        first_name: str,
+        last_name: str,
+        email: str,
+        password: str = None,
+        is_staff=False,
+        is_superuser=False,
+    ) -> "User":
+        if not email:
+            raise ValueError("User must have an email")
+        if not first_name:
+            raise ValueError("User must have a first name")
+        if not last_name:
+            raise ValueError("User must have a last name")
+
+        user = self.model(email=self.normalize_email(email))
+        user.first_name = first_name
+        user.last_name = last_name
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = is_staff
+        user.is_superuser = is_superuser
+        user.save()
+        return user
+
+    def create_superuser(
+        self, first_name: str, last_name: str, email: str, password: str
+    ) -> "User":
+        user = self.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            is_staff=True,
+            is_superuser=True,
+        )
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    first_name = models.CharField(verbose_name="First Name", max_length=30)
+    last_name = models.CharField(verbose_name="Last Name", max_length=30)
+    email = models.EmailField(verbose_name="Email", max_length=100, unique=True)
+    password = models.CharField(max_length=16)
+    username = None
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
